b605ccb1e3a1a768652386c0d759a43ba5ed32a9
Rafael Junior <juniorrafael95@yahoo.com.br>
2019-12-26 13:51:50 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/rafael/cursomc/domain/Cliente.java
+++ src/main/java/com/rafael/cursomc/domain/Cliente.java
@@ -40,6 +40,9 @@ public class Cliente implements Serializable{
 	@CollectionTable(name="telefone")
 	private Set<String> telefones = new HashSet<>();
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<Pedido>();
+	
 	public Cliente() {
 		super();
 	}
@@ -107,6 +110,15 @@ public class Cliente implements Serializable{
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+	
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override

fa6fd936bae096dcaa0c38aa3590866b08f1a8e6
Rafael Junior <juniorrafael95@yahoo.com.br>
2020-01-03 09:35:41 -0300

    Validação customizada, email não repetido em atualização de cliente

--- src/main/java/com/rafael/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/rafael/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.rafael.cursomc.domain.Cliente;
+import com.rafael.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	/**
